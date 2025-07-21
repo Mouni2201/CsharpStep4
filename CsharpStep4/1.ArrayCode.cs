ed093b3e2ef969de719dac10f9ae4e4f15929872
Chanda Mounica <pvryar.4@gmail.com>
2025-07-22 01:11:31 +0530

    Add project files.

---
+++ CsharpStep4/1.ArrayCode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ArrayCode
+{
+    class Arrays
+    {
+        static void Main()
+        {
+            //1.Typesafety: we can create array with same datatype
+
+            int[] arr = new int[2];
+            arr[0] = 1;
+            arr[1] = 2;
+
+            //2.array with fixed size
+
+            int[] arr2 = new int[2];
+            arr2[0] = 3;
+            arr2[1] = 4;
+            //arr2[2] = 5; exception
+            //arr2[3] = 9;
+
+            //3.boxing and unboxing
+            //or no boxing
+
+            int third = arr2[0];
+
+            //4.Multidata types: can't create multiple data types
+            int[] arr3 = new int[2];
+            //arr2[0] = false
+            //arr2[1] = "Hello";
+
+            //5. looping
+            int[] arr4 = new int[4];
+            arr4[0] = 5;
+            arr4[1] = 6;
+            arr4[2] = 7;
+            arr4[3] = 8;
+
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+
+    }
+}
---
+++ CsharpStep4/3.ArrayList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayListExample
+{
+    class ListofArray
+    {
+        static void Main()
+        {
+            ArrayList arrayList = new ArrayList();
+            //here arraylist is predefinedclass in csharp and it can create multiple values
+
+            //1.Non type safety: add any type of data type
+            arrayList.Add(100);
+            arrayList.Add(80);
+            arrayList.Add("Hrello");
+            arrayList.Add(false);
+
+            //2.array list Dynamic size
+            arrayList.Add(100);         //0     object(boxing)
+            arrayList.Add(80);          //1     object
+            arrayList.Add("Hrello");    //2     object
+            arrayList.Add(false);       //3     object
+            arrayList.Add(20);          //4     object
+
+            //object to expected datatype(unboxing)
+
+            //3.boxing or unboxing(required here)
+
+            //if we don't use conversion 
+            int first = (int)arrayList[0];  //unboxing
+
+
+        }
+    }
+}
+
